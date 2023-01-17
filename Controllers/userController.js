106e66cf68916596200bd18ecc9aded1e41479a7
Juan Ferreira <juanandres15@gmail.com>
2023-01-17 01:07:27 +0000

    error on backend after setting up the relationships

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,6 +1,7 @@
 //importing modules
 const bcrypt = require("bcrypt");
 const db = require("../Models");
+
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv").config();
 
@@ -43,41 +44,78 @@ const signup = async (req, res) => {
 
 //login authentication
 
-const login = async (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    //find a user by their email
-    const user = await User.findOne({ email });
-
-    //if user email is found, compare password with bcrypt
-    if (user) {
-      const isSame = await bcrypt.compare(password, user.password);
-
-      //if password is the same
-      //generate token with the user's id and the secretKey in the env file
-
-      if (isSame) {
-        let token = jwt.sign({ id: user.id }, process.env.secretKey, {
-          expiresIn: 1 * 24 * 60 * 60 * 1000,
+const login = (req, res) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json({
+          error: new Error("User not found!"),
         });
-        //if password matches wit the one in the database
-        //go ahead and generate a cookie for the user
-        res.cookie("jwt", token, { maxAge: 1 * 24 * 60 * 60, httpOnly: true });
-        console.log("user", JSON.stringify(user, null, 2));
-        console.log(token);
-        //send user data
-        return res.status(201).send(user);
-      } else {
-        return res.status(401).send("Authentication failed");
       }
-    } else {
-      return res.status(401).send("Authentication failed");
-    }
-  } catch (error) {
-    console.log(error);
-  }
+      bcrypt
+        .compare(req.body.password, user.password)
+        .then((valid) => {
+          if (!valid) {
+            return res.status(401).json({
+              error: new Error("Incorrect password!"),
+            });
+          }
+          const token = jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
+            expiresIn: "24h",
+          });
+          res.status(200).json({
+            userId: user._id,
+            token,
+          });
+        })
+        .catch((error) => {
+          res.status(500).json({
+            error: error,
+          });
+        });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        error: error,
+      });
+    });
 };
+
+// const login = async (req, res) => {
+//   try {
+//     const { email, password } = req.body;
+
+//     //find a user by their email
+//     const user = await User.findOne({ email });
+
+//     //if user email is found, compare password with bcrypt
+//     if (user) {
+//       const isSame = await bcrypt.compare(password, user.password);
+
+//       //if password is the same
+//       //generate token with the user's id and the secretKey in the env file
+
+//       if (isSame) {
+//         let token = jwt.sign({ id: user.id }, process.env.secretKey, {
+//           expiresIn: 1 * 24 * 60 * 60 * 1000,
+//         });
+//         //if password matches wit the one in the database
+//         //go ahead and generate a cookie for the user
+//         res.cookie("jwt", token, { maxAge: 1 * 24 * 60 * 60, httpOnly: true });
+//         console.log("user", JSON.stringify(user, null, 2));
+//         console.log(token);
+//         //send user data
+//         return res.status(201).send(user);
+//       } else {
+//         return res.status(401).send("Authentication failed");
+//       }
+//     } else {
+//       return res.status(401).send("Authentication failed");
+//     }
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
 module.exports = {
   signup,
